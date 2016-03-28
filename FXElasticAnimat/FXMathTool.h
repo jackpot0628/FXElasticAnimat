6a10916ad2fe9b6a5d89bd9b0ff852d91fee28fc
xformax <formaxx@qq.com>
2016-03-28 17:34:25 +0800

    first commit

---
+++ FXElasticAnimat/FXMathTool.h
@@ -0,0 +1,13 @@
+//
+//  FXMathTool.h
+//  FXElasticAnimat
+//
+//  Created by xformax on 16/3/28.
+//  Copyright © 2016年 xformax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FXMathTool : NSObject
+
+@end
---
+++ FXElasticAnimat/FXMathTool.m
@@ -0,0 +1,13 @@
+//
+//  FXMathTool.m
+//  FXElasticAnimat
+//
+//  Created by xformax on 16/3/28.
+//  Copyright © 2016年 xformax. All rights reserved.
+//
+
+#import "FXMathTool.h"
+
+@implementation FXMathTool
+
+@end

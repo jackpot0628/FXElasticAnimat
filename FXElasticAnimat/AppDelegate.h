12ed1f123f9c9a83e7faaba8e1015f70451145ce
xformax <formaxx@qq.com>
2016-03-28 15:23:12 +0800

    Initial Commit

---
+++ FXElasticAnimat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FXElasticAnimat
+//
+//  Created by xformax on 16/3/28.
+//  Copyright © 2016年 xformax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FXElasticAnimat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FXElasticAnimat
+//
+//  Created by xformax on 16/3/28.
+//  Copyright © 2016年 xformax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
